d8e8ab556fba0dec965eedbc896b9c2bb1c418b3
Juan David Cardona <jdavidcardonam@gmail.com>
2021-03-19 23:56:24 -0500

    Desarrollo del segundo punto AKELAB

--- app.js
+++ app.js
@@ -14,3 +14,26 @@ function fibonacci(limite) {
         listaFibo.appendChild(item);
     });
 }
+
+function AKELAB(limite) {
+    let lista = document.getElementById("listaFibonacci");
+    lista.innerHTML = "";
+    let serie = [];
+    for (i = 1; i <= limite; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            serie.push("AKELAB");
+        } else if (i % 3 == 0) {
+            serie.push("AKE");
+        } else if (i % 5 == 0) {
+            serie.push("LAB");
+        } else {
+            serie.push(i);
+        }
+    }
+    serie.forEach((numero) => {
+        let item = document.createElement("li");
+        let textoItem = document.createTextNode(numero);
+        item.appendChild(textoItem);
+        lista.appendChild(item);
+    });
+}
